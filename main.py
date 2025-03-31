500bda6bf2277049d00b358758551b32239e9bce
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-03-30 21:25:35 -0400

    successfully completed L4: Count Words

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-    print(file_contents)
+    #print(file_contents)
+    num_words(file_contents)
+
+
+def num_words(file_contents):
+    words = file_contents.split()
+    wordcount = 0
+    for word in words:
+        wordcount += 1
+    print(wordcount, "words found in the document")
 
 
 def main():
