0b974abcd87ae74c916bffb75e85e9306822ebfd
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-03-31 21:47:12 -0400

    completed CH3: L1: Print a Report

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-def get_book_text(path_to_file):
-    from stats import get_num_words
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    get_num_words(file_contents)
+# Open the book's source location, read the contents and assign it to a string variable
+def get_book_text(file_source):
+    with open(file_source) as f:
+        book_string = f.read()
+    return book_string
 
 
 def main():
+    from stats import get_num_words
+    from stats import get_character_count
+    
     path_to_file = "books/frankenstein.txt"
-    get_book_text(path_to_file)
+    book_string = get_book_text(path_to_file)
+    wordcount = get_num_words(book_string)
+    character_count = get_character_count(book_string)
+
+    # PRINT REPORT
+    print("")
+    print("============ BOOKBOT ============")
+    print("")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("")
+    print("----------- Word Count ----------")
+    print("")
+    print(f"Found {wordcount} total words")
+    print("")
+    print("--------- Character Count -------")
+    print("")
+    for key, value in character_count.items():
+        print(f"{key}: {value}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,23 @@
-def get_num_words(file_contents):
-    words = file_contents.split()
-    wordcount = 0
-    for word in words:
+# Receive in a book in string format, split the string into individual words, and then count how many words are in the list
+def get_num_words(book_string):
+    words = book_string.split()
+    for wordcount in range(0, len(words)):
         wordcount += 1
-    print(wordcount, "words found in the document")
+    return wordcount
+
+
+# Receive in a book in string format, change the text to all lower case, create a dictionary for each character and its qty within the book
+def get_character_count(book_string):
+    # Create an empty dictionary to store the count of each character
+    count_of_char = {}
+    # Change all letters to lowercase
+    no_big_letters = book_string.lower()
+    # Loop through each character in the string
+    for character in no_big_letters:
+        # If the character is already in the dictionary, increment its count
+        if character in count_of_char:
+            count_of_char[character] += 1
+        # Otherwise, add the character to the dictionary with a count of 1
+        else:
+            count_of_char[character] = 1
+    return count_of_char
